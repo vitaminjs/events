712a83a38813960b1b177025c477d14ac85036f0
Abdessamad MOUHASSINE <a.mouhassine@gmail.com>
2016-08-04 09:28:56 +0200

    Update emitter.js

--- src/emitter.js
+++ src/emitter.js
@@ -1,1 +1,18 @@
 
+import Promise from 'bluebird'
+
+// exports
+export default class {
+  
+  /**
+   * EventEmitter class constructor
+   * 
+   * @constructor
+   */
+  constructor() {
+    this._events = {}
+  }
+  
+  
+  
+}
