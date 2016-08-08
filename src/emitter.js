62e20a09f4446b901c730557078bc09843012deb
Abdessamad MOUHASSINE <a.mouhassine@gmail.com>
2016-08-08 10:02:21 +0200

    Update emitter.js

--- src/emitter.js
+++ src/emitter.js
@@ -1,18 +1,57 @@
 
+import EventEmitter from 'events'
 import Promise from 'bluebird'
 
 // exports
-export default class {
+export default class extends EventEmitter {
   
   /**
-   * EventEmitter class constructor
+   * Add a handler for the given event
    * 
-   * @constructor
+   * @param {String} event
+   * @param {Function} fn
+   * @return this
    */
-  constructor() {
-    this._events = {}
+  on(event, fn) {
+    event.split(' ').forEach(name => { name && super.on(name, fn) })
+    return this
   }
   
+  /**
+   * Add a one time handler for the given event
+   * 
+   * @param {String} event
+   * @param {Function} fn
+   * @return this
+   */
+  once(event, fn) {
+    event.split(' ').forEach(name => { name && super.once(name, fn) })
+    return this
+  }
   
+  /**
+   * Trigger sequentially an event with arguments
+   * 
+   * @param {String} event
+   * @param {Array} args
+   * @return promise
+   */
+  emit(event, ...args) {
+    return Promise.reduce(this.listeners(event), (_, fn) => fn(...args), 0)
+  }
+  
+  /**
+   * Remove an event handler
+   * 
+   * @param {String} event
+   * @param {Function} fn
+   * @return this
+   */
+  off(event, fn) {
+    if ( fn ) this.removeListener(...arguments)
+    else this.removeAllListeners(...arguments)
+    
+    return this
+  }
   
 }
